37f348d4dac4cae4470ee0906dbe9397e38f8e6c
Henry Webel <heweb@dtu.dk>
2025-08-21 15:47:32 +0200

    :art: move growth plots to own function (will later be vuecore functionality)

--- app/1_batch_analysis.py
+++ app/1_batch_analysis.py
@@ -1,31 +1,6 @@
-import matplotlib.pyplot as plt
-import pandas as pd
 import streamlit as st
-from matplotlib.dates import DateFormatter
+from plots import plot_growth_data
 from ui_components import is_data_available, show_warning_to_upload_data
-
-
-def plot_growth_data(df: pd.DataFrame):
-    """Plot optical density (OD) growth data."""
-    units = df["pioreactor_unit"].nunique()
-    fig, axes = plt.subplots(units, figsize=(10, 2 * units), sharey=True, sharex=True)
-    # grid container (reactive to UI changes)
-    for (label, group_df), ax in zip(df.groupby("pioreactor_unit"), axes):
-        group_df.plot.scatter(
-            x="timestamp",
-            y="od_reading",
-            rot=45,
-            ax=ax,
-            title=f"Reactor {label}",  # Customize legend text here
-        )
-    ax = axes[-1]
-    fig = ax.get_figure()
-    fig.tight_layout()
-
-    date_form = DateFormatter("%Y-%m-%d %H:%M")
-    _ = ax.xaxis.set_major_formatter(date_form)
-    st.write(fig)
-
 
 ########################################################################################
 # page
@@ -68,12 +43,5 @@ if form_submit and not no_data_uploaded:
     with raw_data:
         st.dataframe(df_raw_od_data, use_container_width=True)
 
-    plot_growth_data(df_raw_od_data)
-
-    # Download options
-    st.download_button(
-        "Download Raw Data",
-        data=df_raw_od_data.to_csv(),
-        file_name="raw_od_data.csv",
-        mime="text/csv",
-    )
+    fig = plot_growth_data(df_raw_od_data)
+    st.write(fig)
---
+++ app/plots.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import streamlit as st
+from matplotlib.dates import DateFormatter
+
+st.cache_data()
+
+
+def plot_growth_data(df: pd.DataFrame):
+    """Plot optical density (OD) growth data."""
+    units = df["pioreactor_unit"].nunique()
+    fig, axes = plt.subplots(units, figsize=(10, 2 * units), sharey=True, sharex=True)
+    # grid container (reactive to UI changes)
+    for (label, group_df), ax in zip(df.groupby("pioreactor_unit"), axes):
+        group_df.plot.scatter(
+            x="timestamp",
+            y="od_reading",
+            rot=45,
+            ax=ax,
+            title=f"Reactor {label}",  # Customize legend text here
+        )
+    ax = axes[-1]
+    fig = ax.get_figure()
+    fig.tight_layout()
+
+    date_form = DateFormatter("%Y-%m-%d %H:%M")
+    _ = ax.xaxis.set_major_formatter(date_form)
+    return fig
