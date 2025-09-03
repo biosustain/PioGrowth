c3f8e56ed0e75f420d1c10d525b94d54dc125488
Henry Webel <heweb@dtu.dk>
2025-09-03 14:57:48 +0200

    :art: clean-up uploading and processing website

--- app/0_upload_data.py
+++ app/0_upload_data.py
@@ -9,7 +9,6 @@ import piogrowth
 
 custom_id = st.session_state["custom_id"]
 df_raw_od_data = st.session_state["df_raw_od_data"]
-rolling_window = st.session_state.get("rolling_window", 31)
 min_periods = st.session_state.get("min_periods", 5)
 
 st.title("Upload Data")
@@ -45,13 +44,44 @@ with st.form("Upload_data_form", clear_on_submit=False):
             "Select reactors to filter",
             options=df_raw_od_data["pioreactor_unit"].unique(),
         )
-    # Options for handeling negative OD readings
-    remove_zero = st.checkbox("Remove zero OD readings", value=False)
     round_time = st.slider("Round time to nearest minute", 0, 15, 5, step=1)
+    # Options for handeling negative OD readings
+    st.write("Data filtering options:")
+    filter_columns = st.columns(3)
+    remove_zero = filter_columns[0].checkbox(
+        "Remove zero OD readings",
+        value=False,
+    )
+    remove_max = filter_columns[1].checkbox(
+        "Remove maximum OD readings by quantile",
+        value=False,
+    )
+    quantile_max = filter_columns[1].slider(
+        "Max quantile for maximum removal",
+        0.9,
+        1.0,
+        0.99,
+        step=0.01,
+    )
+    iqr_range_value = filter_columns[2].slider(
+        "IQR range for outlier removal",
+        1.0,
+        3.0,
+        1.5,
+        step=0.1,
+    )
+    rolling_window = filter_columns[2].slider(
+        "Rolling window (in seconds)",
+        11,
+        61,
+        31,
+        step=2,
+    )
+
     min_date, max_date = None, None
     if df_raw_od_data is not None:
         min_date, max_date = st.select_slider(
-            "Select time window (inferred)",
+            "Select time window (inferred). This won't be plotted as filtered data.",
             options=df_raw_od_data["timestamp_rounded"],
             value=(
                 df_raw_od_data["timestamp_rounded"].min(),
@@ -76,7 +106,7 @@ if custom_id:
 if file is not None:
     df_raw_od_data = piogrowth.load.read_csv(file)
     msg = (
-        f" - Loaded {df_raw_od_data.shape[0]:,d} rows "
+        f"- Loaded {df_raw_od_data.shape[0]:,d} rows "
         f"and {df_raw_od_data.shape[1]:,d} columns.\n"
     )
     # round timestamp data
@@ -107,18 +137,10 @@ if file is not None:
         else:
             df_raw_od_data = df_raw_od_data.loc[mask]
 
-    # Handle negative values
-    if remove_zero:
-        mask_negative = df_raw_od_data["od_reading"] < 0
-        msg += f" - Setting {mask_negative.sum():,d} negative OD readings to NaN.\n"
-        df_raw_od_data.loc[mask_negative, "od_reading"] = np.nan
-
-    st.write(msg)
-
-    st.header(f"Wide OD data with rounded timestamps to {round_time} seconds")
-    # wide data of raw data, maybe removing outliers?
-    # can be used in plot for visualization,
-    # and in curve fitting (where gaps would be interpolated)
+    msg += f"- Wide OD data with rounded timestamps to {round_time} seconds.\n"
+    # wide data of raw data
+    # - can be used in plot for visualization,
+    # - and in curve fitting (where gaps would be interpolated)
     df_wide_raw_od_data = df_raw_od_data.pivot(
         index="timestamp_rounded",
         columns="pioreactor_unit",
@@ -126,29 +148,48 @@ if file is not None:
     )
 
     # skip first or last measurements based on user input (after first loading the data)
+    # ! won't be plotted as filtered data
     if min_date:
         df_wide_raw_od_data = df_wide_raw_od_data.loc[min_date:max_date]
         st.info(f"Time range: {min_date} to {max_date}")
 
-    st.write(df_wide_raw_od_data)
-    # st.write(df_wide_raw_od_data.describe())
-    ax = df_wide_raw_od_data.plot.line(style=".", ms=2)
-    st.write(ax.get_figure())
+    # initalize masked here
+    masked = pd.DataFrame(
+        False,
+        index=df_wide_raw_od_data.index,
+        columns=df_wide_raw_od_data.columns,
+    )
+    df_wide_raw_od_data_filtered = df_wide_raw_od_data.copy()
+
+    # Handle negative values
+    if remove_zero:
+        mask_negative = df_wide_raw_od_data_filtered < 0
+        msg += (
+            f"- Setting {mask_negative.sum().sum():,d} negative OD readings to NaN.\n"
+        )
+        msg += f"   - in detail: {mask_negative.sum().to_dict()}\n"
+        df_wide_raw_od_data_filtered = df_wide_raw_od_data_filtered.mask(mask_negative)
+        masked = masked | mask_negative
+
+    st.title("Processing summary of OD readings")
+    if remove_max:
+        mask_extreme_values = (
+            df_wide_raw_od_data_filtered
+            > df_wide_raw_od_data_filtered.quantile(quantile_max)
+        )
+        msg += (
+            f"- Number of extreme values detected: {mask_extreme_values.sum().sum()}\n"
+        )
+        msg += f"   - in detail: {mask_extreme_values.sum().to_dict()}\n"
+        df_wide_raw_od_data_filtered = df_wide_raw_od_data_filtered.mask(
+            mask_extreme_values
+        )
+        masked = masked | mask_extreme_values
 
     # outlier detection using IQR on rolling window: sets for center value of window a
     # true or false (this would be arguing maybe for long data format)
     # can be used in plot for visualization
     # https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.rolling.html
-
-
-    # set rolling median to x seconds
-    st.title("OD readings with outliers removed")
-    mask_extreme_values = df_wide_raw_od_data > df_wide_raw_od_data.quantile(0.99)
-    st.write(
-        f"### Number of extreme values detected: {mask_extreme_values.sum().sum()}"
-    )
-    st.write(mask_extreme_values.sum())
-    df_wide_raw_od_data_filtered = df_wide_raw_od_data.mask(mask_extreme_values)
 
     mask_outliers = (
         df_wide_raw_od_data_filtered.rolling(
@@ -157,31 +198,24 @@ if file is not None:
             center=True,
             closed="both",
         )
-        .apply(piogrowth.filter.out_of_iqr)
+        .apply(piogrowth.filter.out_of_iqr, kwargs={"factor": iqr_range_value})
         .astype(bool)
     )
-    st.write(f"### Number of outliers detected: {mask_outliers.sum().sum()}")
-    st.write(" - removed extreme values of data before")
-    st.write(mask_outliers.sum())
+    # st.write(f"### Number of outliers detected: {mask_outliers.sum().sum()}")
+    msg += f"- Number of outliers detected: {mask_outliers.sum().sum()}\n"
+    msg += f"   - in detail: {mask_outliers.sum().to_dict()}\n"
+    masked = masked | mask_outliers
 
     # apply mask to entire dataframe
 
     df_wide_raw_od_data_filtered = df_wide_raw_od_data_filtered.mask(mask_outliers)
-    st.write("Filtered OD data:")
-    st.write(df_wide_raw_od_data_filtered)
-    st.write("Summary statistics of filtered data per reactor:")
-    st.write(df_wide_raw_od_data_filtered.describe())
-    ax = df_wide_raw_od_data_filtered.plot.line(
-        style=".",
-        ms=2,
-    )
-    st.write(ax.get_figure())
-    
-    masked = (mask_outliers | mask_extreme_values).convert_dtypes()
-    fpath = Path(f"playground/data/{custom_id}_masked_values.csv")
-    fpath.parent.mkdir(exist_ok=True, parents=True)
-    masked.to_csv(fpath)
-    df_wide_raw_od_data.to_csv(Path(f"playground/data/{custom_id}_raw_wide_data.csv"))
+
+    masked = masked.convert_dtypes()
+    st.write(msg)
+    # fpath = Path(f"playground/data/{custom_id}_masked_values.csv")
+    # fpath.parent.mkdir(exist_ok=True, parents=True)
+    # masked.to_csv(fpath)
+    # df_wide_raw_od_data.to_csv(Path(f"playground/data/{custom_id}_raw_wide_data.csv"))
 
 else:
     with container_download_example:
@@ -214,9 +248,7 @@ if df_raw_od_data is not None:
         center=True,
     ).median()
     st.write(df_rolling)
-    ax = df_rolling.plot.line(
-        style=".", ms=2
-    )
+    ax = df_rolling.plot.line(style=".", ms=2)
     st.write(ax.get_figure())
 
 st.markdown("### Store in QuervE format")
