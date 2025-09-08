03e10960d988ba84ba6331901bd1ea925221a97c
Henry Webel <heweb@dtu.dk>
2025-09-08 13:30:33 +0200

    :art: allow to show raw data, hide data per default
    - add data type for inferred smoothing factor

--- app/1_batch_analysis.py
+++ app/1_batch_analysis.py
@@ -13,29 +13,36 @@ no_data_uploaded = st.session_state.get("df_rolling") is None
 
 if no_data_uploaded:
     show_warning_to_upload_data()
+    st.stop()
 
-st.title("Data used for analysis (rolling median data):")
+df_rolling = st.session_state["df_rolling"].interpolate()
+
+smoothing_range = get_smoothing_range(len(df_rolling))
+
 view_data_module = st.empty()
 with view_data_module:
     st.write("No data available for analysis. Please upload first.")
+
 
 with st.form("Batch_processing_options", enter_to_submit=True):
     correction_strategy = st.radio(
         "Negative correction strategy", ("Median", "Qurve(OD + |min(OD)|)")
     )
     spline_smoothing_values = st.slider(
-        "Smoothing of the spline fitted to OD values (zero means no smoothing)",
-        0.0,
-        1.0,
-        1.0,
-        step=0.05,
+        "Smoothing of the spline fitted to OD values (zero means no smoothing). "
+        "Range suggested using scipy, see "
+        "[docs](https://docs.scipy.org/doc/scipy/reference/generated/scipy.interpolate.make_splrep.html)",
+        1,
+        smoothing_range.s_max,
+        smoothing_range.s_min,
+        step=1,
     )
     high_percentage_treshold = st.slider(
         "Define percentage of µmax considered as high", 0, 100, 90, step=1
     )
     # User inputs for analysis
     st.write("#### Plotting options:")
-    remove_raw_data = st.checkbox("Remove underlying data from plots", value=False)
+    remove_raw_data = st.checkbox("Remove underlying data from plots", value=True)
     add_tangent_of_mu_max = st.checkbox(
         "Add tangent of µmax to growth plots", value=False
     )
@@ -43,15 +50,12 @@ with st.form("Batch_processing_options", enter_to_submit=True):
 
 if not no_data_uploaded:
     with view_data_module:
-        st.dataframe(st.session_state["df_rolling"], use_container_width=True)
+        with st.expander("Data used for analysis (rolling median data):"):
+            st.dataframe(st.session_state["df_rolling"], use_container_width=True)
 
 # Process button
 if form_submit and not no_data_uploaded:
     st.session_state.process_batch = True
-    df_rolling = st.session_state["df_rolling"].interpolate()
-    smoothing_range = get_smoothing_range(len(df_rolling))
-    st.write("Smoothing parameter range as suggested by scipy:")
-    st.dataframe(smoothing_range)
 
     splines, derivatives = fit_spline_and_derivatives_no_nan(
         df_rolling,
@@ -66,12 +70,19 @@ if form_submit and not no_data_uploaded:
     ]
 
     st.title("Fitted splines")
-    st.dataframe(splines, use_container_width=True)
+    with st.expander("Show fitted splines data:"):
+        st.dataframe(splines, use_container_width=True)
     fig, axes = plot_fitted_data(splines, titles=titles)
+    if not remove_raw_data:
+        for col, ax in zip(df_rolling.columns, axes.flatten()):
+            df_rolling[col].plot(
+                ax=ax, c="black", style=".", alpha=0.3, ms=1, label="Raw data"
+            )
     st.write(fig)
 
     st.title("First order derivatives")
-    st.dataframe(derivatives, use_container_width=True)
+    with st.expander("Show first derivative data:"):
+        st.dataframe(derivatives, use_container_width=True)
     fig, axes = plot_derivatives(derivatives=derivatives, titles=titles)
     st.write(fig)
 
--- src/piogrowth/fit.py
+++ src/piogrowth/fit.py
@@ -1,6 +1,10 @@
+from collections import namedtuple
+
 import numpy as np
 import pandas as pd
 from scipy.interpolate import make_splrep, splev
+
+SmoothingRange = namedtuple("SmoothingRange", ["s_min", "s", "s_max"])
 
 
 def get_smoothing_range(m: int):
@@ -8,7 +12,7 @@ def get_smoothing_range(m: int):
     Compute the smoothing range for B-spline fitting in scipy interpolate functionality.
     """
     s_min, s, s_max = int(m - np.sqrt(2 * m)), m, int(m + np.sqrt(2 * m))
-    s = pd.Series([s_min, s, s_max], index=["s_min", "s", "s_max"])
+    s = SmoothingRange([s_min, s, s_max])
     return s
 
 
