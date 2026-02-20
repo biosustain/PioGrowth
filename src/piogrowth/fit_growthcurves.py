2b7c461915fe1a893af21f3fed2eab072bc518e7
Henry Webel <heweb@dtu.dk>
2026-02-20 12:30:27 +0100

    :arrow_up: move to growthcurves 0.6.2

--- src/piogrowth/fit_growthcurves.py
+++ src/piogrowth/fit_growthcurves.py
@@ -8,18 +8,29 @@ def datetimeindex_to_elapsed_hours(index):
     return (index - index[0]).total_seconds() / 3_600
 
 
-def run_model_fitting_on_df(df, model_name="phenom_richards"):
+def run_model_fitting_on_df(
+    df,
+    model_name="phenom_richards",
+    window_points=500,
+    spline_s=1000,
+    n_fits=50,
+    phase_boundary_method=None,
+    **kwargs,
+):
     stats_df = {}
     for col in df.columns:
         s = df[col].dropna()
-        t = datetimeindex_to_elapsed_hours(s.index)
+        t = s.index
         start = time.time()
         _, stats_df[col] = gc.fit_model(
-            time=t.to_numpy(),
-            data=s.to_numpy(),
+            t=t.to_numpy(),
+            N=s.to_numpy(),
             model_name=model_name,
-            window_points=500,
-            spline_s=1000,
+            window_points=window_points,
+            spline_s=spline_s,
+            n_fits=n_fits,
+            phase_boundary_method=phase_boundary_method,
+            **kwargs,
         )
         end_time = time.time()
         elapsed = end_time - start
