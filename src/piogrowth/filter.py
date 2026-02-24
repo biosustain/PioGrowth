892ae369b712f032a2016f5dd7a750d4cec38ee9
Henry Webel <heweb@dtu.dk>
2026-02-24 17:27:09 +0100

    🚧 document subtle differences of using iqr method

--- src/piogrowth/filter.py
+++ src/piogrowth/filter.py
@@ -1,5 +1,7 @@
 import numpy as np
 import pandas as pd
+from growthcurves.preprocessing import out_of_iqr as gc_out_of_iqr
+from growthcurves.preprocessing import out_of_iqr_window
 
 
 def out_of_iqr(s: pd.Series, factor: float = 1.5) -> pd.Series:
@@ -16,3 +18,27 @@ def out_of_iqr(s: pd.Series, factor: float = 1.5) -> pd.Series:
     # center point out of IQR?
 
     return (center < lower_bound) | (center > upper_bound)
+
+
+if __name__ == "__main__":
+    data = pd.Series([20, 1, 2, 3, 4, 5, 20, 6, 7, 8, 9, 10, 20])
+    mask = data.rolling(window=5, center=True).apply(out_of_iqr, raw=False)
+    print(mask)
+
+    # does not work on series due to indexing using values.
+    mask = data.rolling(window=5, center=True).apply(
+        lambda s: out_of_iqr_window(
+            s.values,
+        ),
+        raw=False,
+    )
+    print(mask)
+
+    # would exclude the first and last 2 points as they are NaN, but we want to label
+    # them as outliers if they are out of IQR in their respective windows
+    mask = mask.astype(bool)
+    print(mask)
+
+    # series is understood by numpy and values are accessed.
+    mask = gc_out_of_iqr(data, window_size=5)
+    print(mask)
